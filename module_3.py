fe825241555fe384edbcc2ba3966436d5f6d4ba5
NoyFlat <54071800+NoyFlat@users.noreply.github.com>
2021-08-21 21:22:30 +0300

    Add files via upload

--- module_3.py
+++ module_3.py
@@ -2,19 +2,18 @@ import pandas as pd
 import warnings
 import pickle
 warnings.filterwarnings(action='ignore', category=UserWarning)
+from adacost.adacost import AdaCost
 
 def module_3_model(test_set, model_type):
     test_set = pd.read_csv(fr'{test_set}')
     X, identifier = get_X_and_id(test_set, "identifier")
     clf = pickle.load(open("trained/task_"+str(model_type)+"/classifier_"+str(model_type), "rb"))
     proba = clf.predict_proba(X)[:,1]
-    # auprc = average_precision_score(y, proba)
-    # auc = roc_auc_score(y, proba)
     risk_data = pd.DataFrame({"risk_score": proba})
     output_df = pd.concat([identifier, risk_data], axis=1)
     output_df.to_csv("model_"+str(model_type)+"_mimic_cohort_risk_score_group_2.csv", index=False)
     
-# utils
+# Utils
 
 def get_X_and_id(data_frame, idx_name):
     identifier = data_frame.loc[:,[idx_name]]
